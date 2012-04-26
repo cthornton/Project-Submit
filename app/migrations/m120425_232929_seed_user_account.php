33699dfec701fd738621518782862721322f5fdf
Christopher Thornton <christopher@chris-mint.(none)>
2012-04-25 19:31:11 -0700

    Removed the create admin user

--- app/migrations/m120425_232929_seed_user_account.php
+++ app/migrations/m120425_232929_seed_user_account.php
@@ -4,20 +4,28 @@ class m120425_232929_seed_user_account extends CDbMigration
 {
 	public function up()
 	{
-		echo "Creating Admin account...";
-    $user = new User;
-    $user->username = 'admin';
-    $user->password = '1111qqqq';
-		$user->password_confirmation = '1111qqqq';
-    $user->email    = 'admin@example.com';
-    $user->validated = true;
-		$user->first_name = 'Admin';
-		$user->last_name = 'Account';
-		$user->role_id = 1;
-		if(!$user->save()) {
-			echo "Error with migration!\n";
-			var_dump($user->errors);
-			return false;
+		// Make sure the admin account doesn't exist
+		$findAdmin = User::model()->where('username = "admin"')->findAll();
+		var_dump($findAdmin);
+		if(count($findAdmin) == 0 ) {
+		
+			echo "Creating Admin account...";
+			$user = new User;
+			$user->username = 'admin';
+			$user->password = '1111qqqq';
+			$user->password_confirmation = '1111qqqq';
+			$user->email    = 'admin@example.com';
+			$user->validated = true;
+			$user->first_name = 'Admin';
+			$user->last_name = 'Account';
+			$user->role_id = 1;
+			if(!$user->save()) {
+				echo "Error with migration!\n";
+				var_dump($user->errors);
+				return false;
+			}
+		} else {
+			echo "Admin account already exists; not creating admin user";
 		}
 	}
 
